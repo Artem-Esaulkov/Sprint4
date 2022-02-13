4867ce178713ff6864fecb2188cfd1d3066c6dc7
Artem Esaulkov <a.a.yesaulkov@gmail.com>
2022-02-13 14:19:11 +0500

    fix: Внести последние правки после ревью

--- js/index.js
+++ js/index.js
@@ -5,10 +5,14 @@ let profileName = document.querySelector(".profile__name");
 let profileDescription = document.querySelector(".profile__description");
 let saveProfile = popup.querySelector(".popup__button");
 let formElement = popup.querySelector(".popup__container");
-let overlay = popup.querySelector(".popup__overlay");
+// let overlay = popup.querySelector(".popup__overlay");
+let nameInput = formElement.querySelector(".popup__field_name");
+let descriptionInput = formElement.querySelector(".popup__field_description");
 
 function popupOpened() {
   popup.classList.add("popup_opened");
+  nameInput.setAttribute('value', profileName.textContent);
+  descriptionInput.setAttribute('value', profileDescription.textContent);
 }
 
 function popupClosed() {
@@ -18,17 +22,12 @@ function popupClosed() {
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  let nameInput = formElement.querySelector(".popup__field_name").value;
-  let descriptionInput = formElement.querySelector(
-    ".popup__field_description"
-  ).value;
-
-  profileName.textContent = nameInput;
-  profileDescription.textContent = descriptionInput;
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
   popup.classList.remove("popup_opened");
 }
 
 editButton.addEventListener("click", popupOpened);
 popupClose.addEventListener("click", popupClosed);
-overlay.addEventListener("click", popupClosed);
+// overlay.addEventListener("click", popupClosed);
 formElement.addEventListener("submit", formSubmitHandler);
